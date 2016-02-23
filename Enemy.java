e947015c4a93d0f4edcb7456e46fd28c6a76deb1
Velosoda <velosoda@kean.edu>
2016-02-22 22:59:25 -0500

    made the method in the class more basic for testing reasons

--- Enemy.java
+++ Enemy.java
@@ -5,29 +5,28 @@ public class Enemy {
 	Random random = new Random();
 	int Funds=1000;
 	int soldierCap=5;
+	int r=random.nextInt(soldierCap);
+	int base1Fill=soldierCap-r;
+	int base2Fill=(soldierCap-r-base1Fill)+1;
+	int base3Fill=(soldierCap-(base1Fill+base2Fill));
+	
 	Enemy(){}		
 		
 	//this constructor creates an enemy and fills the bases with random amount of enemy soldiers
 	//might be better to make this into a method so we dont have to create more enemys
 	public void enemyBaseFill(ArrayList<Esoldiers> base1, ArrayList<Esoldiers> base2, ArrayList<Esoldiers> base3){
-		ArrayList <Esoldiers> eSquad = new ArrayList<Esoldiers>(soldierCap);
-		for(int i=0; i<eSquad.size(); i++){
-			eSquad.add(new Esoldiers());
-		}
-		int base1fill=soldierCap-random.nextInt(soldierCap);
-		int base2fill=base1fill-random.nextInt(base1fill);
-		int base3fill=base2fill-random.nextInt(base2fill);
+		int base1Fill=3;
+		int base2Fill=1;
+		int base3Fill=1;
 		//fills base1
-		for(int i=0; i <=base1fill; i++){
-			base1.add(eSquad.get(i));
+		for(int i=0; i<base1Fill; i++){
+			base1.add(i, new Esoldiers());
 		}
-		//fills base2 needs 2 variables one for 0 and one for the loop
-		for(int i=base1fill; i<=base2fill; i++){
-			base2.add(eSquad.get(i));
+		for(int i=0; i<base2Fill; i++){
+			base2.add(i, new Esoldiers());
 		}
-		//fills base3 like base2
-		for(int i=base2fill; i<=base3fill; i++){
-			base3.add(eSquad.get(i));
+		for(int i=0; i<base3Fill; i++){
+			base3.add(i, new Esoldiers());
 		}
 	}
 }
